30939653a8963ab9dfaaa7b7f9cbc65568318f11
sanaehururu1200 <sanaehururu1200@gmail.com>
2017-08-18 22:00:33 +0900

    軽い修正

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -1,7 +1,6 @@
 <?php
 
 /*
-/*
  *
  * __  __            _            _     _       _     _
  *|  \/  | __ _ _ __(_)_ __   ___| |   (_) __ _| |__ | |_ 
@@ -9,26 +8,14 @@
  *| |  | | (_| | |  | | | | |  __/ |___| | (_| | | | | |_ 
  *|_|  |_|\__,_|_|  |_|_| |_|\___|_____|_|\__, |_| |_|\__|
  *                                        |___/
- * == SH - MarineTeam ==
- * == http://marine.otos.red ==
- *
- *  _____            _               _____
- * / ____|          (_)             |  __ \
- *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___
- *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \
- *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
- * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/
- *                         __/ |
- *                        |___/
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author GenisysPro
- * @link https://github.com/GenisysPro/GenisysPro
- *
+ * @auther SH - MarineTeam
+ * @link http://marine.otos.red
  *
 */
 
