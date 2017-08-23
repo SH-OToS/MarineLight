b8e632af04d3c4064bd64aed6bc585e152c8ca56
sanaehururu1200 <sanaehururu1200@gmail.com>
2017-08-23 16:46:24 +0900

    Fix VERSION

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -73,7 +73,7 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "1.1dev";
+	const VERSION = "0.0.1dev";
 	const API_VERSION = "3.0.1";
 	const CODENAME = "Moon";
 	const GENISYS_API_VERSION = '2.0.0';
